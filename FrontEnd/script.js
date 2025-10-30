4d6399f2cff4e24cc3d321352d61c86a0b8a10f2
Julie Gastineau <gastineeaujulie@gmail.com>
2025-10-30 17:57:20 +0100

    creation boutons filtres/categories

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,6 +1,11 @@
+// Affichage des travaux dans la galerie
+
 async function showWorks() {
     try {
         const reponse = await fetch('http://localhost:5678/api/works');
+        if (!reponse.ok) {
+            throw new Error(`Erreur HTTP : ${reponse.status}`);
+        }
         const works = await reponse.json();
     
         const galerie = document.querySelector(".gallery");
@@ -23,5 +28,40 @@ async function showWorks() {
     }
    
 }      
-
+// Appel de la fonction pour afficher les travaux au chargement de la page
 showWorks();
+
+// Filtrage des travaux par catégorie
+
+async function categoriesFilters() {
+    try {
+        const categoriesResponse =await fetch ('http://localhost:5678/api/categories');
+        if (!categoriesResponse.ok) {
+            throw new Error(`Erreur HTTP : ${categoriesResponse.status}`);
+        }
+        const categories = await categoriesResponse.json();
+
+        const filtersContainer = document.createElement("div");
+        filtersContainer.classList.add("filters");
+        const portfolioSection = document.querySelector("#portfolio");
+        portfolioSection.insertBefore(filtersContainer, portfolioSection.firstChild);
+
+        const boutonTous = document.createElement("button");
+        boutonTous.textContent = "Tous";
+        boutonTous.classList.add("filtre-actif");
+        filtersContainer.appendChild(boutonTous);   
+
+        categories.forEach((category) => {
+            const button = document.createElement("button");
+            button.textContent = category.name;
+            button.dataset.id = category.id;
+            filtersContainer.appendChild(button);
+        });
+    } catch (error) {
+        console.error("Erreur lors du chargement des catégories :", error.message);
+    }       
+}
+
+// Appel de la fonction pour afficher les filtres de catégories au chargement de la page
+categoriesFilters();
+
