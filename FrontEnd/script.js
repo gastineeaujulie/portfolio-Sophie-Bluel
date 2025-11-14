90727e64c2219f31895ac51dc990471f31fd79e1
Julie Gastineau <gastineeaujulie@gmail.com>
2025-11-14 19:48:26 +0100

    modal css et javascript

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -15,75 +15,124 @@ async function showWorks() {
         function displayWorks(worksToDisplay) {
             galerie.innerHTML = "";
             worksToDisplay.forEach((work) => {
-            const figure = document.createElement("figure");
-            const img = document.createElement("img");
-            const caption = document.createElement("figcaption");
-        
-            img.src = work.imageUrl;
-            img.alt = work.title;
-            caption.textContent = work.title;
+                const figure = document.createElement("figure");
+                const img = document.createElement("img");
+                const caption = document.createElement("figcaption");
+            
+                img.src = work.imageUrl;
+                img.alt = work.title;
+                caption.textContent = work.title;
 
-            figure.appendChild(img);
-            figure.appendChild(caption);
-            galerie.appendChild(figure);
-        });
-    }   
-    displayWorks(works);
-
-    const filtersContainer = document.createElement("div");
-    filtersContainer.classList.add("filters");
-    const divGallery = document.querySelector(".gallery");
-    const portfolioSection = divGallery.parentNode;
-    portfolioSection.insertBefore(filtersContainer, divGallery);
-
-    const boutonTous = document.createElement("button");
-    boutonTous.textContent = "Tous";
-    boutonTous.classList.add("filtre-actif");
-    filtersContainer.appendChild(boutonTous);
-
-    boutonTous.addEventListener("click", () => {
-        const allButtons = filtersContainer.querySelectorAll("button");
-        allButtons.forEach(btn => btn.classList.remove("filtre-actif"));
-        boutonTous.classList.add("filtre-actif");
+                figure.appendChild(img);
+                figure.appendChild(caption);
+                galerie.appendChild(figure);
+            });
+        }   
         displayWorks(works);
-    });
 
-    categories.forEach((category) => {
-        const button = document.createElement("button");
-        button.textContent = category.name;
-        button.dataset.id = category.id;
-        filtersContainer.appendChild(button);
+        const filtersContainer = document.createElement("div");
+        filtersContainer.classList.add("filters");
+        const divGallery = document.querySelector(".gallery");
+        const portfolioSection = divGallery.parentNode;
+        portfolioSection.insertBefore(filtersContainer, divGallery);
 
-        button.addEventListener("click",() => {
+        const boutonTous = document.createElement("button");
+        boutonTous.textContent = "Tous";
+        boutonTous.classList.add("filtre-actif");
+        filtersContainer.appendChild(boutonTous);
+
+        boutonTous.addEventListener("click", () => {
             const allButtons = filtersContainer.querySelectorAll("button");
             allButtons.forEach(btn => btn.classList.remove("filtre-actif"));
-            button.classList.add("filtre-actif");
-
-            //Filtrer les travaux en fonction de la catégorie sélectionnée
-            const worksFilters = works.filter(work => work.categoryId === category.id);
-            displayWorks(worksFilters);
+            boutonTous.classList.add("filtre-actif");
+            displayWorks(works);
         });
-    });
+
+        categories.forEach((category) => {
+            const button = document.createElement("button");
+            button.textContent = category.name;
+            button.dataset.id = category.id;
+            filtersContainer.appendChild(button);
+
+            button.addEventListener("click",() => {
+                const allButtons = filtersContainer.querySelectorAll("button");
+                allButtons.forEach(btn => btn.classList.remove("filtre-actif"));
+                button.classList.add("filtre-actif");
+
+                //Filtrer les travaux en fonction de la catégorie sélectionnée
+                const worksFilters = works.filter(work => work.categoryId === category.id);
+                displayWorks(worksFilters);
+            });
+        });
     
-    const token = localStorage.getItem("token");
-    if (token) {
-        filtersContainer.style.display = "none";
+        const token = localStorage.getItem("token");
+        if (token) {
+            filtersContainer.style.display = "none";
 
-        const portfolioSection = document.querySelector("#portfolio");
-        const titre = portfolioSection.querySelector("h2");
-        const portfolioHeader = document.createElement("div");
-        portfolioHeader.classList.add("portfolio-header");
-        portfolioSection.insertBefore(portfolioHeader, titre);
-        portfolioHeader.appendChild(titre);
-        const buttonModifier = document.createElement("button");
-        buttonModifier.innerHTML = '<a href="#modal"><i class="fa-regular fa-pen-to-square"></i> modifier</a>';
-        buttonModifier.classList.add("edit-btn");
-        portfolioHeader.appendChild(buttonModifier);
+            const portfolioSection = document.querySelector("#portfolio");
+            const titre = portfolioSection.querySelector("h2");
+            const portfolioHeader = document.createElement("div");
+            portfolioHeader.classList.add("portfolio-header");
+            portfolioSection.insertBefore(portfolioHeader, titre);
+            portfolioHeader.appendChild(titre);
+            const buttonModifier = document.createElement("button");
+            buttonModifier.innerHTML = '<a href="#modal1" class="js-modal"><i class="fa-regular fa-pen-to-square"></i> modifier</a>';
+            buttonModifier.classList.add("edit-btn");
+            portfolioHeader.appendChild(buttonModifier);
 
-    }else{
-        filtersContainer.style.display = "flex";
-        buttonModifier?.remove(); 
-    }
+            let modal = null
+            const focusableSelector = "button, a, input, textarea";
+            let focusables = []
+
+            const openModal = function (e){
+                e.preventDefault()
+                modal= document.querySelector(e.target.getAttribute('href'))
+                modal.style.display = null;
+                modal.removeAttribute('aria-hidden');
+                modal.setAttribute('aria-modal', 'true');
+                modal.addEventListener('click', closeModal)
+                modal.querySelector('.js-modal-close').addEventListener('click', closeModal);
+                modal.querySelector('..js-modal-stop').addEventListener('click', stopPropagation);
+            }
+
+            const closeModal = function (e){
+                if(modal === null) return
+                e.preventDefault()
+                modal.style.display = "none";
+                modal.setAttribute('aria-hidden', 'true');
+                modal.removeAttribute('aria-modal');
+                modal.removeEventListener('click', closeModal);
+                modal.querySelector('.js-modal-close').removeEventListener('click', closeModal);
+                modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation);
+                modal = null;
+            }
+
+            const stopPropagation = function (e) {
+                e.stopPropagation()
+            }
+
+            const focusInModal = function (e) {
+                e.preventDefault()
+        
+            }
+
+            document.querySelectorAll('.js-modal').forEach(a => {
+                a.addEventListener('click', openModal)
+            })
+
+            window.addEventListener('keydown', function (e) {
+                if(e.key === "Escape" || e.key === "Esc") {
+                    closeModal(e)
+                }
+                if(e.key === "Tab" && modal !== null) {
+                    focusInModal(e)
+                }
+            })
+
+        }else{
+            filtersContainer.style.display = "flex";
+            buttonModifier?.remove(); 
+        }
 
     } catch (error) {
         console.error("Erreur lors du chargement des travaux :", error);
